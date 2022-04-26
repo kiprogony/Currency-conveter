cbec8a5890d049f007c4d33d03d176a546007633
Daryl Rogony <darylrogony@gmail.com>
2022-04-26 16:24:48 +0300

    fit: included some selectors in the file

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 const theApi = "https://api.exchangerate-api.com/v4/latest/USD";
+
+let search = document.querySelector(".searchBox");
+let convert = document.querySelector(".convert");
+let fromCurrecy = document.querySelector(".from");
+let toCurrecy = document.querySelector(".to");
+let finalValue = document.querySelector(".finalValue");
+let finalAmount = document.getElementById("finalAmount");
+let resultFrom;
+let resultTo;
+let searchValue;
+  
