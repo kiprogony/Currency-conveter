4bceb2d297c86d8f93b620a92fa0f4b0550baa93
Daryl Rogony <darylrogony@gmail.com>
2022-04-26 16:30:58 +0300

    fit: add a function to get the results

--- index.js
+++ index.js
@@ -14,4 +14,24 @@ let searchValue;
 fromCurrecy.addEventListener('change', (event) => {
     resultFrom = `${event.target.value}`;
 });
+
+toCurrecy.addEventListener('change', (event) => {
+    resultTo = `${event.target.value}`;
+});
+search.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  searchValue = e.target.value;
+}
+
+convert.addEventListener("click",getResults);
+ //function to get the results after convertion
+function displayResults(currency) {
+    let fromRate = currency.rates[resultFrom];
+    let toRate = currency.rates[resultTo];
+    finalValue.innerHTML = 
+       ((toRate / fromRate) * searchValue).toFixed(2);
+    finalAmount.style.display = "block";
+}
+
   
